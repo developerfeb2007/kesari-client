a2cbc7f124cdf122ce1be9a617eb58d58e57a80e
Narendra kumar <narendrakr9870@gmail.com>
2025-04-17 14:19:37 +0530

    image optimization approval

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['localhost'],
-      },
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.NEXT_PUBLIC_HOST_NAME,
+            },
+            {
+                protocol: 'http',
+                hostname: process.env.NEXT_PUBLIC_IMAGE_HOST_NAME,
+            },
+        ],
+    },
 };
 
 export default nextConfig;
